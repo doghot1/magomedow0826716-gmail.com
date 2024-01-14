0aa662ec7ac935173772b3def0399daeda59cd2b
abdulla.m <magomedow0826716@gmail.com>
2024-01-14 21:02:38 +0300

    add plus day1 task1

--- test/mod1/src/Main.java
+++ test/mod1/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        int itaji = input.nextInt();
+        if(itaji <= 4 && itaji > 0){
+            System.out.println("low-store-building))");
+        }if(itaji >= 5 && itaji <=8){
+            System.out.println("mid-rise-building///");
+        }if(itaji >=9){
+            System.out.println("multi-store-building");
+        }if (itaji <=0){
+                System.out.println("mistake");
+            }
+
+
+        }
     }
-}
